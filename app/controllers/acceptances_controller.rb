70595c062163748d2cb52c609cda45c7becd0c08
makoto-niko <mmana19970613@gmail.com>
2024-05-20 12:03:55 +0900

    acceptance C

---
+++ app/controllers/acceptances_controller.rb
@@ -0,0 +1,21 @@
+class AcceptancesController < ApplicationController
+  def new
+    @acceptance = Accepance.new
+  end
+
+  def create
+    @acceptance = Acceptance.new(acceptance_params)
+    if @acceptance valid?
+      puts "承諾されました"
+      redirect to books_path
+    else
+      render :new
+    end 
+  end
+end
+
+private
+ def acceptance_params
+  params.require(:acceptance).permit(:accepting)
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'acceptances/new'
+  get 'acceptances/create'
   root to: "books#index"
   resources :books
 end
